b5136585bb774f06905def4a72f10c1e5f80f1e0
dock0d1 <43358190+dock0d1@users.noreply.github.com>
2022-03-04 22:06:31 -0300

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -8,26 +8,26 @@ from time import sleep
 #cores
 red = "\033[91;1m"
 
-print(red + """
- `.   ``-.                               .-----.._
-   `,     `-.   DNS RESOLVER            .:      /`
-     :       `".. Simple Tool Scan ..-``       :
-     /   ...--:::`n            n.`::...       :
-     `:``      .` ::          /  `.     ``---..:.
-       `\    .`  ._:   .-:   ::    `.     .-``
-         :  :    :_\\_/: :  .::      `.  /
-         : /      \-../:/_.`-`         \ :
-         :: _.._  q` p ` /`             \|
-         :-`    ``(_. ..-----hh``````/-._:
-                     `:      ``     /     `
-                     E:            /
-       [dock]         :          _/
-                      :    _..-``
-                      l--``""")
-sleep(2)
-os.system(['clear', 'cls'][os.name == 'nt'])
+print(red + """  
+    { }
+         {^^,
+         (   `-;
+    _     `;;~~ DNS RESOLVER
+   /(______);
+  (         ( COFFEE IS LIFE
+   |:------( )
+ _//         \\
+/ /          vv
+                      """)
 
-host = input('URL: ')
+def main():
+  sleep(2)
+  os.system(['clear', 'cls'][os.name == 'nt'])
 
-print(red + host, "-->", socket.gethostbyname(host))
-print("Scan Finalizado Com Sucesso!")
+  host = input('URL: ')
+
+  print(red + host, "-->", socket.gethostbyname(host))
+  sleep(1)
+  print("Scan Finalizado Com Sucesso!")
+
+main()
